21b7e9ff9379b0fbcfbc85595e4791b976ddd5af
Yuri The Doer <70323981+eyeri91@users.noreply.github.com>
2023-08-03 11:26:38 +0400

    Make the app PWA

--- src/OtherComponents/Img.tsx
+++ src/OtherComponents/Img.tsx
@@ -2,5 +2,11 @@ import React from "react";
 import aircraftImg from "./aircraftImg.jpg";
 
 export const Image = () => {
-  return <img src={aircraftImg} className="card-img-top" alt="aircraft"></img>;
+  return (
+    <img
+      src={aircraftImg}
+      className="card-img-top img-fluid"
+      alt="aircraft"
+    ></img>
+  );
 };
