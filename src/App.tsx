cd391025e0f6d7794a6265fdf18149cce6a19700
Yuri The Doer <70323981+eyeri91@users.noreply.github.com>
2023-08-19 20:12:18 +0400

    Mute dropdown for temporary

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import "./App.css";
 import Card from "./MainCard/Card";
 import busSchedules from "./Data/busSchedules.json";
-import Dropdown from "./OtherComponents/Dropdown";
+// import Dropdown from "./OtherComponents/Dropdown";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="app d-flex align-items-center justify-content-center min-vh-100">
       <div className="container d-flex flex-column align-items-center justify-content-center">
-        <Dropdown />
+        {/* <Dropdown /> */}
         <Routes>
           <Route path="/" element={<Card {...busSchedules.route6} />} />
           <Route path="/route5" element={<Card {...busSchedules.route5} />} />
